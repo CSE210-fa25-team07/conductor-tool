70e970c22a78a5241ab8dba47513855043331133
ezh247467 <e8huang@ucsd.edu>
2025-10-26 18:36:30 -0700

    Added more rules to ESlint

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,7 @@
 /** @type {import('eslint').FlatConfig[]} */
 module.exports = [
   {
-    ignores: ['node_modules/'],
+    ignores: ["node_modules/"],
 
     languageOptions: {
       ecmaVersion: "latest",
@@ -14,18 +14,29 @@ module.exports = [
     },
 
     rules: {
-      "semi": ["error", "always"],          // enforce semicolons
-      "quotes": ["error", "double"],        // enforce single quotes
-      "indent": ["error", 2],               // 2-space indentation
-      "eqeqeq": ["error", "always"],        // prefer === over ==
-      "no-unused-vars": ["warn"],           // warn for unused variables
-      "no-trailing-spaces": ["error"],      // no trailing spaces
-      "eol-last": ["error", "always"],      // newline at the end of files
-      "comma-dangle": ["error", "never"],   // no trailing commas
-      "no-var": ["error"],                  // prefer let/const
-      "prefer-const": ["error"],            // prefer const over let when possible
-      "space-before-blocks": ["error", "always"],
-      "space-in-parens": ["error", "never"]
+      "semi": ["error", "always"],          		// enforce semicolons
+      "quotes": ["error", "double"],        		// enforce double quotes
+      "indent": ["error", 2],               		// 2-space indentation
+	  "no-duplicate-imports": ["error"],			// import all modules in one line
+      "eqeqeq": ["error", "always"],        		// prefer === over ==
+      "no-unused-vars": ["warn"],           		// warn for unused variables
+      "no-trailing-spaces": ["error"],      		// no trailing spaces
+      "eol-last": ["error", "always"],      		// newline at the end of files
+      "comma-dangle": ["error", "never"],   		// no trailing commas
+      "no-var": ["error"],                  		// prefer let/const
+      "prefer-const": ["error"],            		// prefer const over let when possible
+      "space-before-blocks": ["error", "always"], 	// e.g. if (cond) { <--- if (cond){
+      "space-in-parens": ["error", "never"], 		// e.g. func(a, b) <--- func( a, b )
+	  "no-console" : ["error"],						// no console.log(), etc. in repo
+	  "camelcase": [
+        "error",
+        {
+          properties: "always",
+          ignoreDestructuring: false,
+          ignoreImports: false,
+          ignoreGlobals: false
+        }
+      ]												// enforce camelCase
     }
   }
 ];
