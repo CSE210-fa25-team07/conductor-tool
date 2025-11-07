c85cb653bcc644b5ed29399df75ec036ca2da910
ezh247467 <e8huang@ucsd.edu>
2025-11-07 11:50:48 -0800

    Ironed out details for JSDocs in Infrastructure.md

--- src/sum.js
+++ src/sum.js
@@ -4,6 +4,7 @@
  * @param {number} a - The first number to add.
  * @param {number} b - The second number to add.
  * @returns {number} The sum of 'a' and 'b'.
+ * @memberof module:math
  *
  * @example
  * // Basic usage
