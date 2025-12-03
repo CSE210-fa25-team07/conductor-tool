1a4b2adc5d01b6ec2f31641d7e0ada174dd05855
will7i1am3 <whaung.2003@gmail.com>
2025-12-03 14:06:10 -0800

    Added session check on metric page

--- backend/src/server.js
+++ backend/src/server.js
@@ -46,7 +46,7 @@ app.get("/calendar", checkUserFromSession, (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/html/dashboard/calendar.html"));
 });
 
-app.get("/metrics", (req, res) => {
+app.get("/metrics", checkUserFromSession, (req, res) => {
   res.sendFile(path.join(__dirname, "../../frontend/html/metrics/metrics.html"));
 });
 
